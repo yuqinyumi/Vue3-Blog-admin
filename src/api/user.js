2deb418dee2c048408dff4503a1013c19e933cf4
yuqinyumi <3332014879@qq.com>
2026-04-07 23:26:45 +0800

    登录注册

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+// export const userRegisterService = ({ username, password, repassword }) => {
+//   return request.post('/ai/reg', { username, password, repassword })
+// }
+
+//简写：注册接口
+export const userRegisterService = ({ username, password, repassword }) =>
+  request.post('/api/reg', { username, password, repassword })
+
+export const userLoginService = ({ username, password }) =>
+  request.post('/api/login', { username, password })
